5544965a04c89ca42d37a19256edd3614162dd86
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-01-24 23:16:38 -0600

    Changed color theme and made desktop layout for details more level

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -36,56 +36,61 @@ const GameDetailPage = () => {
 	if (!game) game = { name: "Filler for game name" } as Game;
 
 	return (
-		<SimpleGrid
-			templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
-			gap={6}
-			className="details">
-			<GridItem>
-				<Heading fontSize={"4xl"} as={"h1"} pb={2}>
-					<Skeleton isLoaded={!isLoading}>{game.name}</Skeleton>
-				</Heading>
-				<Skeleton isLoaded={!isLoading}>
-					<Image className="game-cover" src={game.background_image} />
-				</Skeleton>
-				{(!isLoading && (
-					<ExpandableText maxChar={300}>{game.description_raw}</ExpandableText>
-				)) || (
-					<SkeletonText
-						paddingBottom={{ base: "70px" }}
-						noOfLines={{ base: 6, md: 7, lg: 4 }}
-						spacing="3"
-						skeletonHeight="3"
-					/>
-				)}
-				<GameAttributes game={game} isLoading={isLoading} />
-			</GridItem>
-			<GridItem
-				gap={4}
-				display={"flex"}
-				flexDir={"column"}
-				alignItems={"center"}>
-				<Box>
-					<GameTrailer gameId={game.id} />
-					<ScreenshotCarousel gameId={game.id} />
-				</Box>
-				<GameStores gameId={game.id} />
-				{game.website && (
-					<Link to={game.website}>
-						<Button
-							size={{ base: "sm", sm: "xl" }}
-							fontSize={{ base: ".5rem", sm: "1.0rem", lg: "1.3rem" }}
-							marginY={4}
-							padding={4}
-							border="4px double"
-							fontWeight={900}
-							borderColor="gray.500"
-							style={{ textOverflow: "wrap" }}>
-							Learn More About {game.name}
-						</Button>
-					</Link>
-				)}
-			</GridItem>
-		</SimpleGrid>
+		<div>
+			<Heading fontSize={"4xl"} as={"h1"} pb={2}>
+				<Skeleton isLoaded={!isLoading}>{game.name}</Skeleton>
+			</Heading>
+			<SimpleGrid
+				templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
+				templateRows={"repeat(2, 1fr)"}
+				gap={6}
+				className="details">
+				<GridItem>
+					<Skeleton isLoaded={!isLoading}>
+						<Image className="game-cover" src={game.background_image} />
+					</Skeleton>
+					{(!isLoading && (
+						<ExpandableText maxChar={300}>
+							{game.description_raw}
+						</ExpandableText>
+					)) || (
+						<SkeletonText
+							paddingBottom={{ base: "70px" }}
+							noOfLines={{ base: 6, md: 7, lg: 4 }}
+							spacing="3"
+							skeletonHeight="3"
+						/>
+					)}
+					<GameAttributes game={game} isLoading={isLoading} />
+				</GridItem>
+				<GridItem
+					gap={4}
+					display={"flex"}
+					flexDir={"column"}
+					alignItems={"center"}>
+					<Box>
+						<GameTrailer gameId={game.id} />
+						<ScreenshotCarousel gameId={game.id} />
+					</Box>
+					<GameStores gameId={game.id} />
+					{game.website && (
+						<Link to={game.website}>
+							<Button
+								size={{ base: "sm", sm: "xl" }}
+								fontSize={{ base: ".5rem", sm: "1.0rem", lg: "1.3rem" }}
+								marginY={4}
+								padding={4}
+								border="4px double"
+								fontWeight={900}
+								borderColor="gray.500"
+								style={{ textOverflow: "wrap" }}>
+								Learn More About {game.name}
+							</Button>
+						</Link>
+					)}
+				</GridItem>
+			</SimpleGrid>
+		</div>
 	);
 };
 
